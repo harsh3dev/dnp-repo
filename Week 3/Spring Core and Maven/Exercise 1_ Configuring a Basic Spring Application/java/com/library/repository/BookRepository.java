9f8da4c38a788a3aaac9de06266e0f0b6270df01
Harsh Pandey <pandey73harsh@gmail.com>
2025-07-04 11:01:40 +0530

    week3

---
+++ Week 3/Spring Core and Maven/Exercise 1_ Configuring a Basic Spring Application/java/com/library/repository/BookRepository.java
@@ -0,0 +1,6 @@
+package com.library.repository;
+public class BookRepository {
+    public String getBookTitleById(int id) {
+        return "Book ID:" + id + " Book Name:The Courage To Be Disliked";
+    }
+}
